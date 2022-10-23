31903b7716a741d196410e6e04c3b6a53a5a00b0
Manvinder Jawanda <manvinder07@Manvinders-MacBook-Air.local>
2022-10-23 13:29:50 -0700

    Wallet Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Info from "./components/info/Info";
 
 function App() {
+  const [wallet, setWallet] = useState("");
+  const [data, setData] = useState(null);
+  const [transactions, setTransactions] = useState(null);
+
+  function updateWallet(e) {
+    setWallet(e.target.value);
+  }
+
+  function getWalletData(wallet) {
+    fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${wallet}&tag=latest&apikey=${process.env.REACT_APP_API_KEY}`)
+    .then((res) => res.json()).then((data) => {
+      if (data.status == "0") {
+        alert(data.result);
+      } else {
+          setData(data);
+      }
+    });
+
+    fetch(`https://api.etherscan.io/api?module=account&action=txlist&address=${wallet}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${process.env.REACT_APP_API_KEY}`)
+      .then((res) => res.json()).then((transactions) => {
+          setTransactions(transactions);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Ethereum Wallet Tracker</h1>
+      <div className="form">
+        <input type="text" placeholder="0x0" className="input" onChange={updateWallet}></input>
+        <button className="submit" onClick={() => getWalletData(wallet)}>Submit</button>
+        <Info wallet={wallet} data={data} transactions={transactions}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Transactions/Transactions.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./transactions.css";
+
+const Transactions = ({data}) => {
+  return (
+    <div className = "transaction">
+        <p>
+            <b>Block Hash: </b> {data.blockHash}
+        </p>        
+        <p>
+            <b>Block Number: </b> {data.blockNumber}
+        </p>  
+        <p>
+            <b>Transaction Hash: </b> {data.hash}
+        </p>  
+        <p>
+            <b>From: </b> <a href={`https://www.etherscan.io/address/${data.from}`}>{data.from}</a>
+        </p>  
+        <p>
+            <b>To: </b> <a href={`https://www.etherscan.io/address/${data.to}`}>{data.to}</a>
+        </p>      
+        <p>
+            <b>Value: </b> {data.value * 10e-18} ETH
+        </p>   
+        <p>
+            <b>Gas: </b> {data.gas}
+        </p>
+    </div>
+  )
+}
+
+export default Transactions
