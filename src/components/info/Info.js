eb05d5f6af91e4fa0a677641d3d021d947531328
desim <manvinderjawanda@gmail.com>
2025-03-24 23:30:48 -0700

    updated wallet tracker

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Transactions from '../Transactions/Transactions';
+import React from "react";
+import Transactions from "../Transactions/Transactions";
 import "./info.css";
 
-const Info = ({wallet, data, transactions}) => {
-
-
+const Info = ({ wallet, data, transactions }) => {
   return (
     <div className="info">
-        <div className="info-address">
-            <p>
-                Address: <a href={`https://www.etherscan.io/address/${wallet}`}>{wallet}</a>
-            </p>
-        </div>
-        <div className="info-balance">
-            <p>Balance: {data !=== null && data.result * 10e-18 + " ETH"}</p>
-        </div>
-        <h3>Transactions</h3>
-        {transactions?.status === "0" ? "No Transactions" : transactions?.result.
-        map((transaction) => (
-            <Transactions data={transaction}/>
-        ))}
+      <div className="info-address">
+        <p>
+          Address:{" "}
+          <a href={`https://www.etherscan.io/address/${wallet}`}>{wallet}</a>
+        </p>
+      </div>
+      <div className="info-balance">
+        <p>Balance: {data !== null && data.result * 10e-18 + " ETH"}</p>
+      </div>
+      <h3>Transactions</h3>
+      {transactions?.status === "0"
+        ? "No Transactions"
+        : transactions?.result.map((transaction) => (
+            <Transactions data={transaction} />
+          ))}
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
